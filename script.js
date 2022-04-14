eeaaf715d5d38296df026a149916470a83b5aaa0
Albedo <raincadence27@gmail.com>
2022-04-13 23:30:02 -0700

    question right or wrong

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
-var timeLeft = 5;
-const timeLeftDisp = document.getElementById('timer').innerHTML
-const startBtn = document.getElementById('startBtn')
-
-// const timer = () => {
-
-//   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-
-//   setInterval(function() {
-//     timeLeft--
-//     console.log('time down')
-
-//     if (time > 0) {
-//       timeLeftDisp.appendElement(`${timeLeft} seconds left until start`);
-//     }
-
-//     if (timeLeft < 0) {
-//       clearInterval(timeInterval)
-//       window.location.replace("./")
-//     }
-//   }, 1000);
-// }
+//start button timer
 
 function timer() {
-  var sec = 5;
+  var sec = 5
   var timer = setInterval(function () {
-    document.getElementById('timer').innerHTML = '00:0' + sec + ' left until start';
-    sec--;
+    document.getElementById('timer').innerHTML = '00:0' + sec + ' left until start'
+    sec--
     console.log('minus one second')
     if (sec < 0) {
-      clearInterval(timer);
-      window.location.href = "./questionBody.html"
+      clearInterval(timer)
+      const quizCreate = window.location.href = "./questionBody.html"
+      timerQuestions()
     }
-  }, 1000);
+  }, 1000)
 }
 
+const startBtn = document.getElementById('startBtn')
 startBtn.addEventListener('click', function() {
   console.log('start click')
   timer()
 })
+
+//quiz timer
+
+function timerQuestions() {
+  var timeLeft = 60
+  var timer = setInterval(function () {
+    document.getElementById('timer').innerHTML = '00:' + sec + ' left'
+    timeLeft--
+
+    //out of time
+    if (sec < 0) {
+      clearInterval(timer)
+      const endGame = window.location.href = "./highScore.html"
+    }
+  }, 1000)
+}
+
+//question functions
+
+function quizQuestions() {
+  var questionNumber = 1
+}
+
+function rightOrWrong() {
+
+  const answerCorrectBtn = document.getElementById(answerCorrect)
+  answerCorrectBtn.addEventListener('click', function () {
+    console.log('correct click')
+    questionNumber++
+  })
+
+  const answerWrongBtn = document.getElementsByClassName(answerWrong)
+  answerWrongBtn.addEventListener('click', function () {
+    console.log('wrong click')
+    timeLeft - 5
+    questionNumber++
+  })
+}
