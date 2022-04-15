6c08ac76a937f328f3fb8b5d928f77a18ab98fd7
Albedo <raincadence27@gmail.com>
2022-04-14 23:21:33 -0700

    quiz buttons

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 //start button timer
 
 function timer() {
-  var sec = 5
-  var timer = setInterval(function () {
+  let sec = 5
+  let timer = setInterval(function () {
     document.getElementById('timer').innerHTML = '00:0' + sec + ' left until start'
     sec--
     console.log('minus one second')
     if (sec < 0) {
       clearInterval(timer)
       const quizCreate = window.location.href = "./questionBody.html"
-      timerQuestions()
     }
   }, 1000)
 }
 
-const startBtn = document.getElementById('startBtn')
-startBtn.addEventListener('click', function() {
-  console.log('start click')
-  timerQuestions()
-})
-
 //quiz timer
 
+let timeLeft = 60
+
 function timerQuestions() {
-  var timeLeft = 60
   var timerQuiz = setInterval(function () {
     document.getElementById('timerQuiz').innerHTML = `${timeLeft} seconds left`
     timeLeft--
     console.log(timeLeft)
 
     //out of time
-    if (sec < 0) {
+    if (timeLeft < 0) {
       clearInterval(timer)
       const endGame = window.location.href = "./highScore.html"
     }
@@ -39,22 +33,42 @@ function timerQuestions() {
 
 //question functions
 
+let questionNumber = 1
+let quizStart = false
+let answerCorrect
+
 function quizQuestions() {
-  var questionNumber = 1
-
   function rightOrWrong() {
-
     const answerCorrectBtn = document.getElementById(answerCorrect)
     answerCorrectBtn.addEventListener('click', function () {
       console.log('correct click')
       questionNumber++
     })
-
-    const answerWrongBtn = document.getElementsByClassName(answerWrong)
+    const answerWrongBtn = document.getElementById(answerWrong)
     answerWrongBtn.addEventListener('click', function () {
       console.log('wrong click')
       timeLeft - 5
       questionNumber++
     })
   }
+  if (questionNumber=2) {
+    answer 
+    document.getElementById('questionLocation').innerHTML = 'Which is not considered a backend language?'
+    document.getElementById('answer1').innerHTML = 'Javascript'
+    document.getElementById('answer2').innerHTML = 'MongoDB'
+    document.getElementById('answer3').innerHTML = 'SQL'
+    document.getElementById('answer4').innerHTML = 'HTML'
+  }
+}
+
+const startBtn = document.getElementById('startBtn')
+startBtn.addEventListener('click', function () {
+  console.log('start click')
+  timer()
+  quizStart = true
+})
+
+if (quizStart = true) {
+  timerQuestions()
+  quizQuestions()
 }
